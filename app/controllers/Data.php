2d0596c81a817cc7a0a5446fded2e88af775b9b9
Hasan Abdullah <hsn.abdullah282@gmail.com>
2023-04-10 10:23:41 +0700

    create api userdata

--- app/controllers/Data.php
+++ app/controllers/Data.php
@@ -2,36 +2,49 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 class Data extends SELF_Controller
 {
+    private $key;
+
     public function __construct()
     {
         parent::__construct();
-        $this->load->helper(['login', 'error', 'user', 'format']);
+        $this->load->database();
+        $this->load->helper(['api', 'login', 'enkrip']);
+        $this->key = self_md5('surety.ptjis.com');
+        api_header(['GET', 'POST']);
     }
 
     public function index()
     {
-        if (is_login()) {
-            custom_404_admin();
+        http_response_code(404);
+    }
+
+    public function user()
+    {
+        $this->_key_check();
+        $username = $this->input->get('user');
+        $password = self_md5($this->input->get('pass'));
+        $userdata = $this->db->get_where('user', array(
+            'username' => $username, 'password' => $password, 'is_active' => 1
+        ))->row();
+        if ($userdata === null || !method_is('GET')) {
+            bad_request();
         } else {
-            redirect();
+            $data = array(
+                'id' => $userdata->enkripsi,
+                'user' => $userdata->username,
+                'nama' => $userdata->nama,
+                'foto' => base_url('asset/img/user/' . $userdata->photo)
+            );
+            print_data(array($data));
         }
     }
 
-    public function principal()
+    private function _key_check()
     {
-        echo 'Principal';
-
-        // $this->load->model('List_model', 'lists');
-        // $vars = $this->lists->where(array(
-        //     'status' => 2, 'asuransi' => '334322'
-        // ))->order(array('asuransi', 'nomor'))->between(array('0081', '0091'))->query_string();
-        // var_dump($vars);
-
-        // echo $vars['query'];
-    }
-
-    public function obligee()
-    {
-        echo 'Obligee';
+        $key = $this->input->get('key');
+        if ($this->key !== $key) {
+            bad_request();
+            exit;
+        }
     }
 }
--- app/helpers/api_helper.php
+++ app/helpers/api_helper.php
@@ -1,16 +1,19 @@
 <?php
-function api_header()
+
+function api_header($method = ['GET', 'POST', 'DELETE', 'PUT'], $age = false)
 {
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json; charset=UTF-8');
-    header('Access-Control-Allow-Methods: GET, POST, DELETE, PUT');
-    header('Access-Control-Max-Age: 3600');
+    header('Access-Control-Allow-Methods: ' . implode(', ', $method));
+    if ($age !== false) {
+        header('Access-Control-Max-Age: ' . $age);
+    }
     header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
 }
 
-function bad_request()
+function bad_request($code = 400)
 {
-    http_response_code(400);
+    http_response_code($code);
     print json_encode(array('status' => false));
 }
 
